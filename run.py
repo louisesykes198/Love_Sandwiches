7dfdf1a1facb2c1aeced1386c8f2d97c4ea94d61
Louise Sykes <louisesykes198@yahoo.com>
2025-02-02 11:32:09 +0000

    Add while loop and retuen function

--- run.py
+++ run.py
@@ -13,15 +13,21 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
-    
-    print("Please entre sales data from the last market")
-    print("Data should be six numbers, separated by comas")
-    print("Example 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
+    while True:    
+        print("Please entre sales data from the last market")
+        print("Data should be six numbers, separated by comas")
+        print("Example 10,20,30,40,50,60\n")
+
+        data_str = input("Enter your data here: ")
     
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+    return sales_data
 
 def validate_data(values):
     """
@@ -38,5 +44,9 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
 
+        return False
+    
+    return True
 
-get_sales_data()
+
+data = get_sales_data()
